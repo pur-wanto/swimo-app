41ca49090b9d04fa06a47ad1f9e09f30d10495c2
Pur <purwantorpl@gmail.com>
2025-09-16 17:07:04 +0700

    feat: create router

---
+++ src/router/index.tsx
@@ -0,0 +1,29 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+import GetStarted from "../pages/get-started";
+import Login from "../pages/login";
+import SignUp from "../pages/sign-up";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <GetStarted />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUp />,
+      },
+    ],
+  },
+]);
+
+
+export const AppRouter = () => <RouterProvider router={router} />;
