5c5ba993d04193db56ed003b5649c2eb29e50804
Pur <purwantorpl@gmail.com>
2025-09-25 22:47:11 +0700

    feat: initial commit

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import 'antd/dist/reset.css';
+import '@ant-design/v5-patch-for-react-19';
 import { AppRouter } from "./router";
 
 const queryClient = new QueryClient();
