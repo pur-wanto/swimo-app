53f832ae2b35560ad854024d19bef7541535b083
Pur <purwantorpl@gmail.com>
2025-09-25 23:54:28 +0700

    feat: create service loginUser

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,8 +1,9 @@
 import axios from 'axios';
+import { API_BASE_URL } from '../config/env';
 import type { LoginFormValues } from '../validations/loginSchema';
 
 
 export async function loginUser(data: LoginFormValues) {
-  const response = await axios.post('https://swimo-api.duckdns.org/api/v1/sign-in', data);
+  const response = await axios.post(`${API_BASE_URL}/api/v1/sign-in`, data);
   return response.data;
 }
