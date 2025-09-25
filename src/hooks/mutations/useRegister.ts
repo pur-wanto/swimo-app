4ff5eeb648217ce18423877ead7169a118457d66
Pur <purwantorpl@gmail.com>
2025-09-26 00:05:22 +0700

    feat: create hook useRegister

---
+++ src/hooks/mutations/useRegister.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import { useNavigate } from "react-router-dom";
+import { registerUser } from "../../services/auth.service";
+import { message } from "antd";
+
+export function useRegister() {
+  const navigate = useNavigate();
+
+  return useMutation({
+    mutationFn: registerUser,
+    onSuccess: (data) => {
+      message.success('Register sukses!');
+      navigate("/login")
+    },
+    onError: (error: any) => {
+      message.error(error?.response?.data?.message || 'Register gagal');
+    },
+  });
+}
