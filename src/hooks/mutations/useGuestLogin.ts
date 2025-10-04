6490bf0abb77a566ce41b30326b4789cdacb20c6
Pur <purwantorpl@gmail.com>
2025-10-04 22:26:04 +0700

    feat: navigate to dasboard when login as guest

--- src/hooks/mutations/useGuestLogin.ts
+++ src/hooks/mutations/useGuestLogin.ts
@@ -1,14 +1,17 @@
 import { useMutation } from "@tanstack/react-query";
 import { loginGuest } from "../../services/auth.service";
 import useAuthStore from "../../store/authStore";
+import { useNavigate } from "react-router-dom";
 
 export function useGuestLogin() {
   const login = useAuthStore((state) => state.login);
+  const navigate = useNavigate();
 
   return useMutation({
     mutationFn: () => loginGuest(),
     onSuccess: (data) => {
       login(data);
+      navigate("/dashboard");
     },
   });
 }
