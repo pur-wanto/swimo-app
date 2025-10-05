9c1f68720040b83dc46a42e44c9025d14394773b
Pur <purwantorpl@gmail.com>
2025-10-05 16:32:18 +0700

    feat: update login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,90 +1,84 @@
 import { zodResolver } from '@hookform/resolvers/zod';
-import { Button, Input, notification } from 'antd';
-import { Controller, useForm } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
 
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { IconHidePassword, IconShowPassword } from '../assets';
+import Button from '../components/Button';
+import Header from '../components/Header';
 import { useLogin } from '../hooks/mutations/useLogin';
 import { loginSchema, type LoginFormValues } from '../validations/loginSchema';
 
 function Login() {
   const navigate = useNavigate();
   const mutation = useLogin();
+  const [showPassword, setShowPassword] = useState(false);
 
   const {
-    control,
+    register,
     handleSubmit,
     formState: { errors },
   } = useForm<LoginFormValues>({
     resolver: zodResolver(loginSchema),
-    mode: 'onTouched',
+    // mode: 'onSubmit',
     shouldFocusError: false,
-    defaultValues: {
-      email: '',
-      password: '',
-    },
   });
 
   const onSubmit = (data: LoginFormValues) => {
     mutation.mutate(data, {
       onSuccess: () => {
-        navigate("/dashboard");
+        navigate("/dashboard", { replace: true });
       },
-      onError: (error: any) => {
-        const message = error?.response?.data?.message || 'Login gagal. Coba lagi.';
-        notification.error({
-          message: 'Login Error',
-          description: message,
-        });
-      }
     });
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100 px-4">
-      <div className="w-full max-w-md p-6 bg-white rounded shadow">
-        <h3 className="text-center mb-6">Login</h3>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <div className="w-full max-w-md bg-white rounded shadow">
+        <Header onClick={() => navigate("/")} />
+        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4 p-6">
           <h5>Email</h5>
-          <Controller
-            name="email"
-            control={control}
-            render={({ field }) => (
-              <Input
-                {...field}
-                size="large"
-                placeholder="Input your Email"
-              />
-            )}
+          <input
+            type="email"
+            {...register("email")}
+            className="p-2 border border-gray-600 rounded w-full"
+            placeholder="Input your Email"
           />
           {errors.email && (
             <p className="text-red-500 text-sm mt-1">{errors.email.message}</p>
           )}
+
           <div className='mt-1' />
+
           <h5>Password</h5>
-          <Controller
-            name="password"
-            control={control}
-            render={({ field }) => (
-              <Input.Password
-                {...field}
-                size="large"
-                placeholder="Input your Password"
+          <div className="relative">
+            <input
+              type={showPassword ? "text" : "password"}
+              {...register("password")}
+              className='p-2 border border-gray-600 rounded w-full'
+              placeholder="Input your Password"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(prev => !prev)}
+              className="absolute right-2 top-1/2 transform -translate-y-1/2 text-sm text-gray-600"
+            >
+              <img
+                src={showPassword ? IconHidePassword : IconShowPassword}
+                className="w-4 h-4 object-cover"
+                alt="toggle password visibility"
               />
-            )}
-          />
+            </button>
+          </div>
           {errors.password && (
             <p className="text-red-500 text-sm mt-1">{errors.password.message}</p>
           )}
-
+          <div className="h-2" />
           <Button
-            type="primary"
-            size="large"
-            block
+            title='Login'
             htmlType="submit"
-            loading={mutation.isPending}
-          >
-            Login
-          </Button>
+            type='primary'
+          />
         </form>
 
         <div className='flex justify-center items-center mt-4 space-x-2'>
