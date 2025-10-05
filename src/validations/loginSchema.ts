934857b791860380f87d9157e7b414bc3acbe8c9
Pur <purwantorpl@gmail.com>
2025-10-05 16:31:30 +0700

    chore: update min length password

--- src/validations/loginSchema.ts
+++ src/validations/loginSchema.ts
@@ -2,11 +2,10 @@ import { z } from 'zod';
 
 export const loginSchema = z.object({
   email: z
-    .string()
     .email({ message: 'Invalid email address' }),
   password: z
     .string()
-    .min(8, { message: 'Password must be at least 8 characters long' }),
+    .min(6, { message: 'Password must be at least 6 characters long' }),
 });
 
 export type LoginFormValues = z.infer<typeof loginSchema>;
